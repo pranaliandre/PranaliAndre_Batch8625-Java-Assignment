a7301c19c7f68887d7fb55729c6f19c67d0fb33a
pranaliandre <53554744+pranaliandre@users.noreply.github.com>
2021-12-01 21:53:49 +0530

    encapsulation program

---
+++ encapsulation.java
@@ -0,0 +1,16 @@
+public class Data {
+private String name;
+public String getName() {
+return this.name;
+}
+public void setName(String name) {
+this.format = name;
+}
+}
+public class Main {
+Public static void main(String[] main){
+Data d = new Data()
+d.setName(“Wiz”);
+System.out.println(d.getName());
+}
+}
